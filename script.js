343cc1ed047f2d869d704bbba175f485e8bd0a65
mahhriana123 <143807489+mahhriana123@users.noreply.github.com>
2025-06-27 22:50:08 -0300

    aula 27/06

---
+++ script.js
@@ -0,0 +1,25 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelector('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentarTamanho;
+
+function diminuiTamanho(){
+    //tamanhoSenha = tamanhoSenha-1;
+    if (tamanhoSenha > 1){
+        tamanhoSenha--;
+         numeroSenha.textContent = tamanhoSenha
+    }
+   
+}
+
+function aumentarTamanho(){
+    if (tamanhoSenha > 20){
+        tamanhoSenha++;
+        numeroSenha.textContent = tamanhoSenha
+    }
+    }
